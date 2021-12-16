16cea7b39881aa8591321146019c9be26cb143ab
Greg Doud <gpdoud@gmail.com>
2021-12-15 20:39:38 -0500

    Committed by GitUtil

--- src/main/java/com/maxtrain/capstone/prs/user/UserRepository.java
+++ src/main/java/com/maxtrain/capstone/prs/user/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface UserRepository extends JpaRepository<User, Integer> {
 	User findByUsernameAndPassword(String username, String password);
 	
-	@Query("select u from User u where u.username = ?1 and u.password = ?1")
+	@Query("select u from User u where u.username = ?1 and u.password = ?2")
 	User findByLogin(String username, String password);
 	
 	@Query("select u from User u where u.lastname = ?1")
